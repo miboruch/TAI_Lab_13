eb54b1f853f1f27f44e508fdbca5a9e94bb94df7
Michał Boruch <miboruch@gmail.com>
2020-06-21 14:43:27 +0200

    Fixed scss imports

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent implements OnInit {
   public post = {};
